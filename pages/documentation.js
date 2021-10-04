8ab549778516b1164a7b6bdd7a9573ec89ad6e22
Vincent <vincentp.ruel@gmail.com>
2021-10-04 11:19:13 -0400

    Correction of the centerContainer class in the global scss file

--- pages/documentation.js
+++ pages/documentation.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 
+import styles from '../styles/pages/Documentation.module.scss'
+
+
 export default function Documentation() {
   return (
     <div>
@@ -7,8 +10,16 @@ export default function Documentation() {
         <title>Avantage Numérique - Documentation complète</title>
       </Head>
 
-      <h1>Page de documentation</h1>
+      <div className="centerContainer">
+        <header>
 
+          <h1>Ceci est le titre de la page</h1>
+        </header>
+        <section>
+          <p>Etiam vel ultrices sapien. Donec ipsum lorem, pharetra non commodo imperdiet, facilisis bibendum quam. Suspendisse consequat tellus massa, ut venenatis lacus rutrum at. Fusce sit amet lectus dui. Donec ut mauris eu nulla porta rutrum quis ornare diam. Nulla sed quam eget magna hendrerit vulputate non porta eros.</p>
+        </section>
+      </div>
+      
     </div>
   )
 }
