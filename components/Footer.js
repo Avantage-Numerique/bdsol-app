8069affd9616396763f3bd0adb624282f6792402
Vincent <vincentp.ruel@gmail.com>
2021-11-05 14:31:50 -0400

    first developpement of the page footer

--- components/Footer.js
+++ components/Footer.js
@@ -10,10 +10,52 @@ const Footer = () => {
 
     return (
 
-        <footer id={footerStyles.pageFooter}>
+        <footer id={footerStyles.pageFooter} className="dark_BG">
+            {/* center the contain of the page */}
             <div className="maxWidthPageContainer">
 
-                <small>&copy; <a href="https://avantagenumerique.org/">Avantage Numérique</a> - 2021</small>
+                {/* 
+                *
+                    News letter section 
+                *
+                */}
+                <section className={"col-12"}>
+                    <div className={"col-6"}>
+                        <h2 className={"col-12"}>Abonnez-vous à notre infolettre</h2>
+                        <p className={"col-12"}>Restez au courant de tous les projets d'Avantage Numérique en vous abonnant à notre infolettre.</p>
+                    </div>
+                    <div className={"col-6"}>
+                        <form className={"col-12"}>
+                            <input placeholder="Courriel" />
+                            <input type="submit"/>
+                        </form> 
+                    </div>
+                </section >
+
+                {/* 
+                *
+                    Project partners section
+                *
+                */}
+                <section className={"col-12"}>
+
+                    <h2 className={"col-12"}>Nos partenaires</h2>
+
+                    <h2 className={"col-12"}>Suivez nous</h2>
+
+
+                </section>
+
+                {/* 
+                *
+                    Logo section
+                *
+                */}
+                <section className={"col-12"}>
+                    <small>&copy; <a href="https://avantagenumerique.org/">Avantage Numérique</a> - 2021</small>
+
+                </section>
+
 
             </div>
         </footer>
