e167a2394c3f545713250a492d7297766c3dcd43
SonofaPancak <epaulard21@hotmail.com>
2022-08-08 16:05:26 -0400

    Implémentation du select pour lié taxonomy fonctionnel pour createPerson, mais à revérifier, et à ajouter au form org

--- app/common/FormElements/Select/Select.js
+++ app/common/FormElements/Select/Select.js
@@ -1,10 +1,15 @@
-import React, {useEffect, useState, useReducer} from 'react'
+import React, {useEffect, useState, useReducer, useRef} from 'react'
 import {useHttpClient} from '../../../../app/hooks/http-hook'
 import Button from '../Buttons/Button/Button'
 //Styling
 import styles from './Select.module.scss'
 
 const Select = (props) => {
+
+    const selectTagRef = useRef(null);
+
+    //Extract the functions inside useHttpClient
+    const {sendRequest} = useHttpClient();
 
     const inputReducer = (state, action) => {
         return {
@@ -14,19 +19,17 @@ const Select = (props) => {
         };
     }
 
-    //Extract the functions inside useHttpClient
-    const {sendRequest} = useHttpClient();
-
     const [inputState, dispatch] = useReducer(inputReducer, {
-        value: '', 
+        value: [], 
         isValid: true
     });
 
-    const [SelectList, setSelectList] = useState([]);
+    const [selectList, setSelectList] = useState([]);
+    const [selectedItems, setSelectedItems] = useState([]);
 
     useEffect(() => {
         const getSelectList = async () => {
-            if(props.request != undefined && props.requestData != undefined){
+            if(props.request != undefined && props.requestData != undefined) {
             const SelectList =  await sendRequest(
                 props.request,
                 'POST',
@@ -39,7 +42,6 @@ const Select = (props) => {
         getSelectList();
     },[]);
     
-    const SelectedItem = ["Pompier", "Architecte", "Joueur de soccer"];
     const { name, onInput } = props;
     const { value, isValid } = inputState;   //State of this element
 
@@ -47,44 +49,61 @@ const Select = (props) => {
         onInput(name, value, isValid)
     }, [name, value, isValid, onInput]);
 
-    const changeHandler = event => {
-        SelectList.data.forEach(occupation => {
-            if (event.target.value == occupation.name)
-                event.target.occId = occupation._id;
+    const addValueToSelectedItem = () => {
+        selectList.data.forEach(item => {
+            if (selectTagRef.current.value == item.name && !selectedItems.includes(item)) {
+                const tempSelect = selectedItems;
+                tempSelect.push(item);
+                setSelectedItems(tempSelect);
+                selectTagRef.current.value = "";
+                selectTagRef.current.focus(); //Reset focus on field
+            }
         });
-        if (event.target.occId != "")
-            dispatch({val: event.target.occId, isValid:true});
+        dispatch({val: selectedItems, isValid:true});
         //For the moment, this only send the id if the occupation exist
         //If taxonomy schema expect and id, when the value is 12 char long, the cast to objectId succeed even tho it shouldn't. Beware
-        else
-            dispatch({val: "", isValid:true});
-        event.target.occId = "";
     }
 
-    if( SelectList &&
-        !SelectList.error &&
-        SelectList.data &&
-        SelectList.data.length > 0)
+    const removeValueFromSelectedItem = (select) => {
+        console.log(select);
+        let tempTag=[];
+        selectedItems.forEach(item => {
+            if(item.name != select.name)
+                tempTag.push(item);
+        })
+        //console.log(selectedItems);
+        //console.log(tempTag);
+        setSelectedItems(tempTag);
+        //Vincent! J'aimerais que le state se mette à jour avec le "setSelectedItems" au lieu de devoir faire dispatch
+        //Si je ne fais pas dispatch il s'update seulement si j'add qqch
+        //Et pk si je met val: selectedItems sa fonctionne pas, je dois obligatoirement mettre tempTag
+        dispatch({val: tempTag, isValid:true});
+    }
+
+    if( selectList &&
+        !selectList.error &&
+        selectList.data &&
+        selectList.data.length > 0)
     return (
         <>
-            <label for='occupation'>{props.label}</label>
+            <label for='SelectInput'>{props.label}</label>
             <br/>
             <div>
-                <Button slim="true">+</Button>
-                <input occId="" type="text" list='occupationsDatalist' name='occupationInput'
-                    id='occupationInput' placeholder=' "Enseignant", "Architecte logiciel", [...]'
-                    className={`${styles["datalist-input"]}`} onChange={changeHandler}/>
-                <datalist id='occupationsDatalist' name="occupationsDatalist" className={`${styles["datalist-input"]}`}>
-                    {SelectList.data.map( occ => 
-                        <option value={occ.name}></option>
+                <Button type="button" slim="true" onClick={addValueToSelectedItem}>+</Button>
+                <input type="text" list='SelectDatalist' name='SelectInput'
+                    id='SelectInput' placeholder=' "Enseignant", "Architecte logiciel", [...]'
+                    className={`${styles["selectInput"]}`} ref={selectTagRef}/>
+                <datalist id='SelectDatalist' name="SelectDatalist" className={`${styles["datalist-input"]}`}>
+                    {selectList.data.map( item => 
+                        <option value={item.name}></option>
                     )}
                 </datalist>
             </div>
             <div className={`${styles['tagList']}`}>
-                {SelectedItem.map(selected =>
+                {selectedItems.map(selected =>
                 <>
-                    <input type="submit" value="x" className={`${styles['input']}`}></input>
-                    <label className={`${styles['label']}`}>{selected}</label>
+                    <input className={`${styles['tagInput']}`} type="button" onClick={() => removeValueFromSelectedItem(selected)} value="x"></input>
+                    <tag className={`${styles['tag']}`}>{selected.name}</tag>
                 </>
                 )}
             </div>
