734499732f294e928d6060055e6debfdb8d819a2
SonofaPancak <epaulard21@hotmail.com>
2022-08-26 13:55:23 -0400

    Mise en forme et correctif sur la page d'aide du compte

--- app/common/Containers/UserAccount/Help/help.js
+++ app/common/Containers/UserAccount/Help/help.js
@@ -1,37 +1,39 @@
 
+//Styling
+import styles from './help.module.scss'
 
 const Help = () => {
     return (
-        <>
-            <h3>Bienvenue dans l'espace utilisateur !</h3>
-            <p>
+        <div className={`col-12 ${styles["help-menu"]}`}>
+            <h2>Bienvenue dans l'espace utilisateur !</h2>
+            <div>
                 Tout d'abord, merci de tester la BDSOL.
                 Vos commentaires nous aiderons à améliorer l'application pour la mettre à votre image!
-            </p>
-            <p>
-                Un bouton nommé "Partage d'idée" se trouve sur la droite de votre écran. Il permet de prendre en image une partie de votre écran 
-                et de surligner des éléments. Cette interface vous permettra d'écrire vos commentaires, ainsi que de mentionner des éléments ou 
-                fonctionnalitée qui, selon vous, sont manquant à l'application :
-            </p>
-            <p>
-                Pour facilité votre navigation, voici les différentes interfaces qui vous aiderons à naviguer l'application.
-                    <ol>
-                        <b>Dans la barre, en haut de l'écran :</b>
-                        <li>Le bouton Avantage Numérique vous ramènera à la page d'accueil</li>
-                        <li>Un peu plus à droite, se trouve un bouton rond de "profil" indiquant le menu du compte pour vous déconnecter et accéder à la page compte, où vous vous trouvez présentement</li>
-                        <li>Totalement à droite se trouve un bouton vous indiquant un menu de navigation qui vous permettera de facilement naviguer vers la page que vous souhaitez</li>
-                    </ol>
-                    <ol>
-                        <b>À la droite de ce texte, la section Menu</b><br></br>
-                        <b>Chacun des boutons de ce menu vous présentera différentes sections de la page compte</b>
-                        <li><b>"Aide" :</b> affiche ce texte de présentation</li>
-                        <li><b>"Modifier mon profil" :</b> encore en construction, vous permettera dans le futur de modifier les informations de votre compte</li>
-                        <li><b>"Historique de modification" :</b> affichera les modifications que vous avez apportée à la base de données <br></br>
-                            (Les ajouts, modifications ou suppression)</li>
-                        <li><b>"Se déconnecter" :</b> vous l'aurez compris, permet de vous déconnecter. Cependant vous trouverez aussi la possibilité de vous déconnecter dans le bouton profil au haut de la page</li>
-                    </ol>
-            </p>
-        </>
+            </div>
+            <div>
+                Un bouton nommé "<u>Partage d'idée</u>" se trouve sur la droite de votre écran. Il permet de prendre en image une partie de votre écran 
+                et de surligner des éléments. Cette interface vous permettra d'écrire vos commentaires ainsi que de nous partager des éléments ou 
+                fonctionnalités que vous aimeriez voir dans l'application.
+            </div>
+            <div>
+                <h3>Pour facilité votre navigation</h3>
+                <h4>Dans la barre, au haut de l'écran :</h4>
+                <ul>
+                    <li>- Le bouton <u>Avantage Numérique</u> vous ramènera à la page d'accueil.</li><br></br>
+                    <li>- Un peu plus à droite, se trouve un <u>bouton rond de "profil"</u> permettant d'accéder à la page du compte, où vous vous trouvez présentement.</li><br></br>
+                    <li>- Totalement à droite, le bouton fait de <u>3 lignes horizontales</u> vous montre un menu qui facilitera la navigation vers les autres pages.</li>
+                </ul>
+                <br></br>
+                <h4>À la droite de ce texte, la section Menu du compte</h4>
+                <ul>
+                    <li>- "<u>Aide</u>" : affiche ce texte de présentation</li><br></br>
+                    <li>- "<u>Modifier mon profil</u>" : encore en construction, vous permettera dans le futur de modifier les informations de votre compte</li><br></br>
+                    <li>- "<u>Historique de modification</u>" : affichera les modifications que vous avez apportée à la base de données (Les ajouts, modifications ou suppression)</li><br></br>
+                    <li>- "<u>Se déconnecter</u>" : vous l'aurez compris, permet de vous déconnecter. Cependant vous trouverez aussi la possibilité de vous déconnecter dans le bouton profil au haut de la page</li>
+                </ul>
+                <br></br>
+            </div>
+        </div>
     )
 }
 
