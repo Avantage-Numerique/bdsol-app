919d80d2e01b51a6f504ee0eddd878b9cf7abc6d
Vincent <vincentp.ruel@gmail.com>
2022-08-29 21:35:35 -0400

    Ajout d'éléments dans la page de consultation des personnes

--- DataTypes/Person/Components/Views/MainPersonView/MainPersonView.js
+++ DataTypes/Person/Components/Views/MainPersonView/MainPersonView.js
@@ -12,7 +12,7 @@ const MainPersonView = ({ data }) => {
     console.log(data)
 
     return (
-        <article className={`${styles["main-person-piew"]}`}>
+        <article className={`${styles["main-person-view"]}`}>
             
             {/*
             *
@@ -24,44 +24,102 @@ const MainPersonView = ({ data }) => {
                 {/* Background image */}
                 <figure>
                     <img src="/general_images\forestBG.jpg" alt="Background image for a person card"/>
+                    <div className={`${styles["black-gradient"]}`}></div>
                 </figure>
 
                 {/* Text content */}
                 <div className={`${styles["headers-text"]}`}>
-                    <div></div>
-                    <div></div>
-                    <div>                    
-                        <h1>Le beau Jonathan</h1>
+                    <div>
+                        <button> &#8629; Retour </button>
+                    </div>
+                    <div className={`${styles["headers-text__main-section"]}`}>
+                        <div className="col-8">
+                            <h2>Jonathan Champoin</h2>
+                            <p>shampoo boy</p>
+                        </div>
+                        <aside className="col-4">
+                            <div className="col-10">comsacsa</div>
+                        </aside>
+                    </div>
+                    <div>
+                        <figure className={`${styles["headers-text__profil-picture"]}`}>
+                            <img src="/general_images/Dennis_Nedry.webp" alt="Background image for a person card" />
+                        </figure>
                     </div>
 
                 </div>
 
-
             </header>
+
+            {/*
+            *
+            *      Quick informations
+            * 
+            */}
+
+            <section className={`${styles["quick-section"]}`}>
+
+                <div className={`${styles["quick-section__single-info"]}`}>
+                    <span>Langue : </span>Français
+                </div>
+
+                <div className={`${styles["quick-section__single-info"]}`}>
+                    <span>citoyenneté : </span>Canadienne
+                </div>
+
+                <div className={`${styles["quick-section__single-info"]}`}>
+                    <span>Langue : </span>Français
+                </div>
+
+            </section>
+
 
             {/*
             *
             *  MAIN SECTION
             * 
             */}
-            <section>
+            <section className={`col-8 ${styles["main-section"]}`}>
 
-                <div className={`${styles["main-section"]}`}>
 
-                    <div className={`${styles["main-section__description"]}`}>
-                        <h4>Présentation</h4>
-                        <SanitizedInnerHtml>
-                            texte descriptif
-                        </SanitizedInnerHtml>
-                    </div>
-
-                    <div className={`${styles["main-section__line"]}`}></div>
-
-                    <div className={`${styles["main-section__singular-infos"]}`}></div>
-
+                <div className={`${styles["main-section__single-info"]}`}>
+                    <h4>
+                        Présentation
+                    </h4>
+                    <SanitizedInnerHtml>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla laoreet orci, in scelerisque eros accumsan non. Ut lorem elit, rutrum vitae ornare quis, iaculis ac nisl. Sed sit amet pulvinar purus. Pellentesque orci massa.
+                    </SanitizedInnerHtml>
                 </div>
 
+                <div className={`${styles["main-section__single-info"]}`}>
+                    <h4>
+                        Présentation
+                    </h4>
+                    <SanitizedInnerHtml>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla laoreet orci, in scelerisque eros accumsan non. Ut lorem elit, rutrum vitae ornare quis, iaculis ac nisl. Sed sit amet pulvinar purus. Pellentesque orci massa.
+                    </SanitizedInnerHtml>
+                </div>
+
+                <div className={`${styles["main-section__complex-info"]}`}>
+                    <h4>
+                        Présentation
+                    </h4>
+                    <SanitizedInnerHtml>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla laoreet orci, in scelerisque eros accumsan non. Ut lorem elit, rutrum vitae ornare quis, iaculis ac nisl. Sed sit amet pulvinar purus. Pellentesque orci massa.
+                    </SanitizedInnerHtml>
+                </div>
+
+
+
             </section>
+
+            <aside className="col-3">
+
+                <h4>Adresse</h4>
+                <h4>Moyen de contact</h4>
+
+
+            </aside>
 
 
         </article>
