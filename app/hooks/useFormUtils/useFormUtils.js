89a2489c212d874b76fc88db0cb8b52f6a47e362
Vincent <vincentp.ruel@gmail.com>
2022-09-19 10:05:59 -0400

    Retrait des commentaires dans le code

--- app/hooks/useFormUtils/useFormUtils.js
+++ app/hooks/useFormUtils/useFormUtils.js
@@ -106,46 +106,7 @@ export const useFormUtils = ( initialState, action ) => {
 
     }, [innerMessage, isLoading])
 
-    //State element for a message displayed inside the form
 
     return { FormUI, submitRequest, formState, formTools,  }
 
 }
-
-/*
-
-Brain storm !!!! 
-
-    Qu'est-ce qu'on veut centraliser comme fonctions : 
-
-        L'appel des messages externes
-        L'appel des messages internes
-        Effacage des formulaires lorsque nécessaires
-        Toutes les autres fonctions d'édition du state
-        Affichage du spinner et du message
-        L'envoie de requêtes
-
-        Si je fais toutes ces méthodes, il faut avoir
-        des manières de sauter des étapes
-
-        Options 1 : 
-            Entièrement automatisée
-            Gère l'envoie de requêtes et le spinner
-            Pourrait automatiquement effacer le contenu du formulaire lorsque positif
-            Gère les messages. 
-            Pourrait avoir des input pour
-            exécuter du code dans le cas d'un réponse positive
-            Pourrait 
-
-            Action :
-                1. If positive, than ask for a redirection link. If there is none, by default empty the form. Answer with global message
-                2. If negative, display the errors with inner message.
-
-
-        Si on considère que le component de formulaire
-        doit être aussi simple que possible pour être scalable
-        Il pourrait justement y avoir un component 
-        par dessus qui pourrait recevoir toutes les options. 
-
-        const [formWrapper, sendRequest] = useForm() 
-*/
