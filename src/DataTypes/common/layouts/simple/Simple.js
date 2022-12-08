9d9218d1d7475b6551d19e8ab34e6969cd645f8f
Vincent <vincentp.ruel@gmail.com>
2022-12-08 12:10:12 -0500

    Création d'un component Simple dans le répertoire 'commun' des datatypes. Ceci permettra de l'utiliser pour appliquer des styles commun aux component simple propres à chaque entité

---
+++ src/DataTypes/common/layouts/simple/Simple.js
@@ -0,0 +1,20 @@
+import React from 'react' 
+
+import styles from './Simple.module.scss'
+
+
+/*
+        This component is intended to receive and apply all the rules
+        and styling common to all the entities's simple component 
+*/
+
+const Simple = () => {
+
+    return (
+        <article className={`${styles.simple}`}>
+
+        </article>
+    )
+}
+
+export default Simple
