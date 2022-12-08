a5e255796a03c8ac003d78f42580a292240b2e8c
Vincent <vincentp.ruel@gmail.com>
2022-12-08 12:13:43 -0500

    Création d'un component Single identique au component simple, lequel offre une forme de base aux futurs simple components de chaque entité

---
+++ src/DataTypes/common/layouts/single/Single.js
@@ -0,0 +1,20 @@
+import React from 'react' 
+
+import styles from './Single.module.scss'
+
+
+/*
+        This component is intended to receive and apply all the rules
+        and styling common to all the entities's single component 
+*/
+
+const Single = () => {
+
+    return (
+        <article className={`${styles.single}`}>
+
+        </article>
+    )
+}
+
+export default Single
