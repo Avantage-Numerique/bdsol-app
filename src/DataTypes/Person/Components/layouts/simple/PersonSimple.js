e3d82342d34954bcb04699df71626457b9632df8
Vincent <vincentp.ruel@gmail.com>
2022-12-30 12:16:07 -0500

    Ajout des derniers détails pour le moment sur le component Simple de l'entité Person

--- src/DataTypes/Person/Components/layouts/simple/PersonSimple.js
+++ src/DataTypes/Person/Components/layouts/simple/PersonSimple.js
@@ -1,4 +1,7 @@
 import React from "react"
+import Link from 'next/link'
+import getConfig from 'next/config';
+
 
 /***  Components  ****/
 import Simple from '@/DataTypes/common/layouts/simple/Simple'
@@ -6,8 +9,9 @@ import Simple from '@/DataTypes/common/layouts/simple/Simple'
 /***  Local styling ***/
 import styles from './PersonSimple.module.scss'
 
-
 const PersonSimple = ({ data, key, header }) => {
+
+    const { publicRuntimeConfig } = getConfig();
 
     const {
         slug,
@@ -18,9 +22,12 @@ const PersonSimple = ({ data, key, header }) => {
         username,
         description,
         createdAt,
+        occupations,
         url,
         contactPoint
     } = data
+
+    console.log("occupations", occupations)
 
     return (
         <Simple className={`${styles["person-simple"]}`}>
@@ -35,35 +42,67 @@ const PersonSimple = ({ data, key, header }) => {
                         <img src={"/general_images/Dennis_Nedry.webp"} alt={`Photo de profil de l'utilisateur ${firstName} ${lastName}`} />
                     </figure>
                     {/* Button to see more details */}
-                    <button className={`d-flex ${styles["person-simple__header__button"]}`}>
-                        <svg 
-                            className="h-100"
-                            xmlns="http://www.w3.org/2000/svg" 
-                            viewBox="0 0 10 36">
-                                <circle cx="5" cy="5" r="4"/>
-                                <circle cx="5" cy="18" r="4"/>
-                                <circle cx="5" cy="31" r="4"/>
-                        </svg>
-                    </button>
+                    <Link href={`/persons/${slug}`}>
+                        <button 
+                            className={`d-flex ${styles["person-simple__header__button"]}`}>
+                            <svg 
+                                className="h-100"
+                                xmlns="http://www.w3.org/2000/svg" 
+                                viewBox="0 0 10 36">
+                                    <circle cx="5" cy="5" r="4"/>
+                                    <circle cx="5" cy="18" r="4"/>
+                                    <circle cx="5" cy="31" r="4"/>
+                            </svg>
+                        </button>
+                    </Link>
                 </section>
                 {/* Header's text and infos */}
                 <section className={`${styles["person-simple__header__bottom-section"]}`}>
                     <h3 className="text-center h4 mb-1">{firstName} {lastName}</h3>
                     {nickname && <h4 className="text-center h5 text-secondary fw-normal">{nickname}</h4>}
-                    <ul className={`mt-3 w-100 d-inline-flex flex-wrap ${styles["occupations-container"]}`}>
-                        <li className={`mw-100 py-1 px-2 border border-secondary text-secondary rounded-4 d-flex text-truncate text-nowrap ${styles["occupation"]}`}><div className="text-truncate">Développeur Web</div></li>
-                        <li className={`mw-100 py-1 px-2 border border-secondary text-secondary rounded-4 d-flex text-truncate text-nowrap ${styles["occupation"]}`}><div className="text-truncate">Designer graphique</div></li>
-                        <li className={`mw-100 py-1 px-2 border border-secondary text-secondary rounded-4 d-flex text-truncate text-nowrap ${styles["occupation"]}`}><div className="text-truncate">Chanteur de</div></li>
-                    </ul>
+ 
+                    {/* Display the three first occupations, then three dots to reprensent that there are others */}
+                    {occupations && <ul className={`mt-3 w-100 d-inline-flex flex-wrap ${styles["occupations-container"]}`}>
+                        {
+                            occupations.map((elem, index) => (
+                                <>
+                                {   index < 3 &&
+                                    <li key={"occ" + elem._id} className={`mw-100 py-1 px-2 border border-secondary text-secondary rounded-4 d-flex text-truncate text-nowrap ${styles["occupation"]}`}><div className="text-truncate">{elem.occupation.name}</div></li>
+                                }
+                                {   index == 3 &&
+                                    <li key="occ999999999999" className={`mw-100 py-1 px-2 border border-secondary text-secondary rounded-4 d-flex text-truncate text-nowrap ${styles["occupation"]}`}><div className="text-truncate">...</div></li>
+                                }
+                                </>
+                            ))
+                        }
+                    </ul>}
+                        
                 </section>
 
             </header>
             <div className="border-bottom my-1"></div>
-            <section>
-            
-                
-            </section>
 
+            {/* 
+                <small>
+                    <time className="">
+                        {(new Date(createdAt)).toLocaleDateString(publicRuntimeConfig.dates.defaultLanguage)}
+                    </time>
+                </small>
+            */}
+            <footer className={`${styles["person-single__footer"]}`}>
+                <div className="d-flex justify-content-between">
+                    <small>
+                        <time>
+                            {(new Date(createdAt)).toLocaleDateString(publicRuntimeConfig.dates.defaultLanguage)}
+                        </time>
+                    </small>
+                    <small>
+                        <time>
+                            {(new Date(createdAt)).toLocaleTimeString(publicRuntimeConfig.dates.defaultLanguage)}
+                        </time>
+                    </small>
+                </div>
+            </footer>
 
         </Simple>
     )
