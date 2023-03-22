fe16ec35ce69e8f2ceb5671947eca3b5e8f477c8
mamarmite <marcandre@mamarmite.com>
2023-03-22 10:31:41 -0400

    Ajout de la documentation pour le component Icon.

--- src/common/widgets/Icon/Icon.js
+++ src/common/widgets/Icon/Icon.js
@@ -1,4 +1,13 @@
-
+/**
+ * Add an icon with a tag i with line awesome as default supported classes.
+ * @param props.iconName {string} Name of the target icon to add, without the prefix and vendor class.
+ * @param props.classClass {string} additionnal class to add as is in the element className property.
+ * @param props.vendor {string} It's the class that setup the target icon font, default is las for line awesome.
+ * @param props.prefix {string} It,s the icon name class's prefix, default is for line awesome.
+ * @param props.Tag {Component||string de tag} Tag to use for the icon, default <i>
+ * @return {JSX.Element}
+ * @constructor
+ */
 const Icon = (props) => {
 
     let {
@@ -11,13 +20,14 @@ const Icon = (props) => {
 
     Tag = Tag ?? 'i';
     className = className ?? '';
-    //line awesome parameters.
     vendor = vendor ?? 'las';
     prefix = prefix ?? 'la';
 
+    const prefixSep = "-";
+
     return (
-        <Tag className={`${vendor} ${prefix}-${iconName} ${className}`}></Tag>
+        <Tag className={`${vendor} ${prefix}${prefixSep}${iconName} ${className}`}></Tag>
     )
 }
 
-export default Icon
+export default Icon;
