f53118d3e5797127e9f6813e19e5b4d113a4b461
Vincent <vincentp.ruel@gmail.com>
2023-03-22 19:46:14 -0400

    Ajustement du style du formulaire de création de goupes de compétences

--- src/DataTypes/common/Forms/UpdateSkillGroup/UpdateSkillGroup.js
+++ src/DataTypes/common/Forms/UpdateSkillGroup/UpdateSkillGroup.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect } from 'react';
 
 //hooks
 import { useModal } from '@/src/hooks/useModal/useModal';
@@ -9,6 +9,10 @@ import { useFormUtils } from '@/src/hooks/useFormUtils/useFormUtils';
 import Button from "@/FormElements/Button/Button"
 import Input from '@/src/common/FormElements/Input/Input';
 import Select2Tag from '@/src/common/FormElements/Select2/Select2Tag';
+import Icon from '@/src/common/widgets/Icon/Icon';
+
+//styling
+import styles from './UpdateSkillGroup.module.scss';
 
 
 
@@ -20,35 +24,68 @@ const Group = () => {
             isValid: false
         },
         skillList: {
+            value: [],
+            isValid: true
+        },
+        groupName1: {
             value: "",
+            isValid: false
+        },
+        skillList1: {
+            value: [],
+            isValid: true
+        },
+        groupName2: {
+            value: "",
+            isValid: false
+        },
+        skillList2: {
+            value: [],
             isValid: true
         }
     })
 
+    useEffect(() => {
+
+        console.log("formState", formState)
+
+    }, [formState])
+
     return (
-        <article className="row border border-secondary p-2 my-2">
+        <article className={`
+            row border border-1 rounded p-2 my-2 bg-white
+            ${styles["update-skill-group"]}
+        `}>
             {/* Icone to move the element */}
             <div className="col flex-grow-0 fs-2 text-secondary">
-                &#x292D;
+                <Icon iconName={"arrows-alt-v"} />
             </div>
             {/* Content of the elements */}
-            <section className="col">
-                <div className="d-flex justify-content-between align-items-end my-2"> 
-                    <h4>Nom du groupe</h4>
-                    <Button color="danger" size="slim">
+            <section className={`
+                col
+                ${styles["skill-group-inputs-container"]}
+            `}>
+                <div className="d-flex justify-content-between align-items-end my-2">
+                    <h4>Occupation / groupe de compétences</h4> 
+                    <Button type="button" color="danger" size="slim">
                         &#x2716;
                     </Button>
                 </div>
                 <Input 
+                    label="Nom de groupe"
                     name="groupName"
+                    validationRules={[
+                        {name: "REQUIRED"}
+                    ]}
                     formTools={formTools}
                 />
                 <Select2Tag
+                    label="Attribuer des compétences"
                     searchField="name"
                     fetch="/taxonomies/list"
                     requestData={{category:"occupations", name:""}}
                     name="skillList"
-                    idField="offer"
+                    idField="skill"
                     formTools={formTools}
                 />
             </section>
