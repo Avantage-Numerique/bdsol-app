8351baa3aabc2485616d396449c879a5a3090662
Vincent <vincentp.ruel@gmail.com>
2023-03-28 20:50:27 -0400

    Terminé la v1 du repeater. Il permet de créer des minis formulaires qui, lorsque mis à jour, transmettent l'info aux formulaire principal

---
+++ src/DataTypes/common/Forms/SkillGroupRepeater/SkillGroupRepeater.js
@@ -0,0 +1,171 @@
+import {useState, useEffect, useRef, useCallback} from 'react';
+import React from 'react';
+
+import { useFormUtils } from '@/src/hooks/useFormUtils/useFormUtils';
+import Button from "@/FormElements/Button/Button"
+
+
+const iterateOverChildren = (children, formInitStructure, formTools) => {
+    return React.Children.map((children), (child) => {
+      // equal to (if (child == null || typeof child == 'string'))
+      if (!React.isValidElement(child)) return child;
+      //If this child has a name prop, and if the value is equal to one of the values declared for the formState, then it means it is a field and require a formtool
+      const newProps = Object.keys(formInitStructure).some(key => key === child.props?.name) ? {'formTools': formTools} : {};
+      return React.cloneElement(child, {
+        ...child.props,
+        ...newProps,
+        // you can alse read child original className by child.props.className
+        children: iterateOverChildren(child.props.children, formInitStructure, formTools)})
+    })
+};
+
+const RepeaterSingleIteration = ({children, formInitStructure, iterationKey, updateIterationValue}) => {
+
+        //Create its own sub form state
+        const {formState, formTools} = useFormUtils(formInitStructure);
+
+        useEffect(() => {
+            // update the value
+            updateIterationValue(iterationKey, formState.inputs, formState.isValid);
+         }, [formState])
+
+        return iterateOverChildren(children, formInitStructure, formTools);
+}
+
+//Util function to get a key in an object based on its value
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+
+const SkillGroupRepeater = props => {
+
+    /*
+        List of props 
+    */
+    const {
+        children,               //Elements to repeat
+        formInitStructure,
+        mainFormTools,
+        name,
+        formReturnStructure
+    } = props;
+
+    const {
+        formState,
+        inputHandler
+    } = mainFormTools;
+
+    //const currentState = formState.inputs[name];
+    //Value of the current state : {value: Array(0), isValid: true}
+    //The main form is going to look out for the isValid property to let the user know if the state can be submited or not.
+
+    //State to manage the values of every iterations of the repeater
+    const [iterations, setIterations] = useState(createIteration());
+
+    //Whenever the iteration state change, apply the changes on the main state
+    useEffect(() => {
+
+        //Data to return in the main form state
+        let isValid = true
+        let value = [];
+
+        //Convert the iterations elements into an array 
+        const arrayOfIterationsValues = iterations ? Object.values(iterations) : [];
+        //Loop through it to look at every children
+        arrayOfIterationsValues.forEach(ite => {
+            //If only one isn't valid, the hole feild becomes invalid
+            if(!ite.value.isValid)
+                isValid = false;
+            //Shape the entered values
+            let returnShape = {...formReturnStructure}
+            //Create an array of keys to exame based on the inital structure
+            const arrayOfNames = Object.keys(formInitStructure);
+            //Loop through it
+            arrayOfNames.forEach(fieldName => {
+                //replace the name by the value to return
+                returnShape[getKeyByValue(returnShape, fieldName)] = ite.value[fieldName] ? ite.value[fieldName].value : {};
+            })
+            //Add the result to the return array
+            value.push(returnShape);
+        })
+
+        inputHandler(name, value, isValid)
+
+    }, [iterations])
+
+    /*************
+     * 
+     * Functions
+     * 
+     ****/
+    function updateIterationValue(key, value, isValid) {
+        setIterations({
+            ...iterations,
+            [key]: {
+                ...iterations[key],
+                value: value,
+                isValid: isValid
+            }
+        })
+    }
+
+
+    function addNewIteration(){
+        const newValue = createIteration();
+        setIterations({
+            ...iterations,
+            ...newValue
+        })
+    }
+
+    //Create a new Id and make sure its not gonna be in double
+    function generateUniqueId(){
+        //Create a new Id
+        const newId = Math.floor((Math.random() * 100000) + 1);
+        //Make sure it doesn't exist already into the state. Also accept is iteration is not defined yet
+        if(!iterations || !Object.keys(iterations).some(key => key == newId))
+            return newId;
+        //If the id isn't unique, than call the function again
+        generateUniqueId();
+    }
+
+    //Create a new Id and make sure its not gonna be in double
+    function createIteration(){
+        //Create an ID
+        const key = generateUniqueId();
+        //Build the object
+        const obj = {
+            [key]: {
+                key: key,
+                order: 1,  //not used for now
+                value: {},
+                isValid: true
+            }
+        }
+        //Return it
+        return obj;
+    }
+
+    return (
+        <>  
+            <section>
+               { iterations && Object.values(iterations).map(iteration => (
+                    <RepeaterSingleIteration 
+                        key={iteration.key} 
+                        iterationKey={iteration.key}
+                        formInitStructure={formInitStructure}
+                        updateIterationValue={updateIterationValue}
+                    >
+                        {children}
+                    </RepeaterSingleIteration>
+               ))}                   
+            </section>
+            <div className="d-flex justify-content-end">
+                <Button type="button" onClick={addNewIteration} className="m-0">Ajouter</Button>
+            </div>
+        </>
+    )
+}
+
+export default SkillGroupRepeater
--- src/DataTypes/common/Forms/UpdateSkillGroup/UpdateSkillGroup.js
+++ src/DataTypes/common/Forms/UpdateSkillGroup/UpdateSkillGroup.js
@@ -14,90 +14,18 @@ import Icon from '@/src/common/widgets/Icon/Icon';
 //styling
 import styles from './UpdateSkillGroup.module.scss';
 
-
-
-const Group = () => {
-
-    const {FormUI, submitRequest, formState, formTools} = useFormUtils({
-        groupName: {
-            value: "",
-            isValid: false
-        },
-        skillList: {
-            value: [],
-            isValid: true
-        },
-        groupName1: {
-            value: "",
-            isValid: false
-        },
-        skillList1: {
-            value: [],
-            isValid: true
-        },
-        groupName2: {
-            value: "",
-            isValid: false
-        },
-        skillList2: {
-            value: [],
-            isValid: true
-        }
-    })
-
-    useEffect(() => {
-
-        console.log("formState", formState)
-
-    }, [formState])
-
-    return (
-        <article className={`
-            row border border-1 rounded p-2 my-2 bg-white
-            ${styles["update-skill-group"]}
-        `}>
-            {/* Icone to move the element */}
-            <div className="col flex-grow-0 fs-2 text-secondary">
-                <Icon iconName={"arrows-alt-v"} />
-            </div>
-            {/* Content of the elements */}
-            <section className={`
-                col
-                ${styles["skill-group-inputs-container"]}
-            `}>
-                <div className="d-flex justify-content-between align-items-end my-2">
-                    <h4>Occupation / groupe de compétences</h4> 
-                    <Button type="button" color="danger" size="slim">
-                        &#x2716;
-                    </Button>
-                </div>
-                <Input 
-                    label="Nom de groupe"
-                    name="groupName"
-                    validationRules={[
-                        {name: "REQUIRED"}
-                    ]}
-                    formTools={formTools}
-                />
-                <Select2Tag
-                    label="Attribuer des compétences"
-                    searchField="name"
-                    fetch="/taxonomies/list"
-                    requestData={{category:"occupations", name:""}}
-                    name="skillList"
-                    idField="skill"
-                    formTools={formTools}
-                />
-            </section>
-        </article>
-    )
-}
+import SkillGroupRepeater from '../SkillGroupRepeater/SkillGroupRepeater';
 
 const UpdateSkillGroup = () => {
 
     const { Modal, closeModal } = useModal();
 
-
+    const {FormUI, submitRequest, formState, formTools} = useFormUtils({
+        skillGoups: {
+            value: [],
+            isValid: true
+        },
+    })
 
     return (
         <> 
@@ -105,8 +33,64 @@ const UpdateSkillGroup = () => {
 
                 <form className="w-100">
 
-                    <Group />
-                    <Group />
+                <SkillGroupRepeater
+                    mainFormTools={formTools}
+                    name="skillGoups"
+                    formInitStructure={{
+                        groupName: {
+                            value: "",
+                            isValid: false
+                        },
+                        skillList: {
+                            value: [],
+                            isValid: true
+                        }
+                    }}
+                    formReturnStructure={{
+                        groupName: "groupName",
+                        skillList: "skillList"
+                    }}
+                >
+
+                    <article className={`
+                        row border border-1 rounded p-2 my-2 bg-white
+                        ${styles["update-skill-group"]}
+                    `}>
+                        {/* Icone to move the element */}
+                        <div className="col flex-grow-0 fs-2 text-secondary">
+                            <Icon iconName={"arrows-alt-v"} />
+                        </div>
+                        {/* Content of the elements */}
+                        <section className={`
+                            col
+                            ${styles["skill-group-inputs-container"]}
+                        `}>
+                            <div className="d-flex justify-content-between align-items-end my-2">
+                                <h4>Occupation / groupe de compétences</h4> 
+                                <Button type="button" color="danger" size="slim">
+                                    &#x2716;
+                                </Button>
+                            </div>
+                            <Input 
+                                label="Nom de groupe"
+                                name="groupName"
+                                validationRules={[
+                                    {name: "REQUIRED"}
+                                ]}
+                            />
+                            <Select2Tag
+                                label="Attribuer des compétences"
+                                searchField="name"
+                                fetch="/taxonomies/list"
+                                requestData={{category:"occupations", name:""}}
+                                name="skillList"
+                                idField="skill"
+                            />
+                        </section>
+                    </article>
+
+                </SkillGroupRepeater>
+
 
 
                 </form>
