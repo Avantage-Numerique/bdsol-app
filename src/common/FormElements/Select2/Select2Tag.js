322ab6aa7e3481941e893c78d01f84a73cd3f457
SonofaPancak <epaulard21@hotmail.com>
2023-04-24 12:40:32 -0400

    Implémentation du selectFetch qui fetch les options à afficher dans le select

--- src/common/FormElements/Select2/Select2Tag.js
+++ src/common/FormElements/Select2/Select2Tag.js
@@ -46,8 +46,6 @@ const Select2Tag = ({name, formTools, ...props}) => {
     //List of options fetched by the api and proposed to the user
     const [selectResponse, setSelectResponse] = useState([]);
     const [optionList, setOptionList] = useState([]);
-    console.log(optionList)
-    console.log(selectResponse)
 
     const selectRef = useRef();
     const [inputValue, setInputValue] = useState("");
