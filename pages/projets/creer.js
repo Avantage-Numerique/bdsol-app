ce19431083b02f34636063b6b5fc5411bf7af2c6
mamarmite <marcandre@mamarmite.com>
2023-05-04 15:05:28 -0400

    (fix) : du build avec le nom de l'import pour CreateProjectForm dans la page creer.js

--- pages/projets/creer.js
+++ pages/projets/creer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 //Component
 import Single from "@/DataTypes/common/layouts/single/Single";
-import CreateProject from '@/src/DataTypes/Project/forms/CreateProjectForm';
+import CreateProjectForm from '@/src/DataTypes/Project/forms/CreateProjectForm';
 
 //Custom hooks
 import {useModal} from "@/src/hooks/useModal/useModal";
