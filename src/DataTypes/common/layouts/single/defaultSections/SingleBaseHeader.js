3e6e96d07e09ba9ca0f858d0c33eb109663bbb16
Vincent <vincentp.ruel@gmail.com>
2023-05-17 13:33:04 -0400

    Correction du singlePersonView

--- src/DataTypes/common/layouts/single/defaultSections/SingleBaseHeader.js
+++ src/DataTypes/common/layouts/single/defaultSections/SingleBaseHeader.js
@@ -18,27 +18,26 @@ import MainImageDisplay from '@/DataTypes/common/layouts/single/defaultSections/
  */
 
 const SingleBaseHeader = (props) => {
-    
+
     const {
         mainImage,
+        entity,
         title,
         subtitle,
         type
     } = props;
 
-
-
     return (
         <section className="row position-relative p-4 ms-0">
             <div className="col-md-6 order-2 order-md-1">
                 { /* title */ }
-                {title ?? <h2 className='mt-4 ms-4'>Titre</h2>}
+                { title ?? <h2 className='mt-4 ms-4'>Titre</h2>}
 
                 { /* subtitle */ }
                 { subtitle ?? <h3 className='ms-4'>Sous-titre</h3> }
 
                 { /* mainImage */ }
-                <MainImageDisplay mainImage={mainImage} />
+                <MainImageDisplay mainImage={mainImage} entity={entity} />
 
             </div>
             <div className="col-md-6 order-1 order-md-2 d-flex flex-md-column align-items-end justify-content-between">
