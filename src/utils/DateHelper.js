4d2e24e31be1f5fc2a3caedb40e3671b75ea89aa
mamarmite <marcandre@mamarmite.com>
2023-06-02 11:38:39 -0400

    (Modification) : skills vérifie dans tout les .* (Mopdification) : date helper ne fait rien si ce n'est pas un string.

--- src/utils/DateHelper.js
+++ src/utils/DateHelper.js
@@ -1,5 +1,8 @@
 
 export const getDateFromIsoString = (isoString) => {
-    //const date = new Date(isoString);//in the format iso ; '2018-04-20T12:00:00Z'
-    return isoString.split('T')[0];
+    if (typeof isoString === "string") {
+        //const date = new Date(isoString);//in the format iso ; '2018-04-20T12:00:00Z'
+        return isoString.split('T')[0];
+    }
+    return isoString;
 }
