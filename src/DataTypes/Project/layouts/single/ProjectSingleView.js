d6ff746363168cd95892dedde1d88c1e2742ba15
SonofaPancak <epaulard21@hotmail.com>
2023-06-13 15:25:01 -0400

    Ajout du champ domains dans project à l'édition et à la vue single

--- src/DataTypes/Project/layouts/single/ProjectSingleView.js
+++ src/DataTypes/Project/layouts/single/ProjectSingleView.js
@@ -33,6 +33,7 @@ const ProjectSingleView = ({ data }) => {
         sponsor,
         scheduleBudget,
         skills,
+        domains,
         context,
         status,
         createdAt,
@@ -178,6 +179,15 @@ const ProjectSingleView = ({ data }) => {
                 </>
             }
             </SingleInfo>
+
+            <SingleInfo title={lang.domainsSingleLabel} className={"mb-3"}>
+                <SearchTag
+                    className="row"
+                    list={domains}
+                    listProperty={"domain"}
+                />
+            </SingleInfo>
+            
             <SingleInfo 
                 title={"Contact"} 
                 className={"mb-3"}
