9def7b0be221c27a70a6cd3c9fcf7dad992965ee
mamarmite <marcandre@mamarmite.com>
2023-11-17 18:33:06 -0500

    (ajout) de la politique de confidentialité + les terms of use version 0.5 (ajout) du component pour ajouter un lien interne via les AppRoutes

--- src/configs/AppConfig.js
+++ src/configs/AppConfig.js
@@ -1,5 +1,19 @@
 export const appConfig = {
     name : "AVNU",
+    url : {
+        uri: "https://avnu.ca",
+        label: "AVNU.ca"
+    },
+    legal: {
+        email: "bonjour@avnu.ca",
+        phone: "1 (819) 797-6436",
+        adresse: "7e rue, Rouyn-Noranda, (QC), J9X 1Z9"
+    },
+    support: {
+        email: "bonjour@avnu.ca",
+        phone:"1 (819) 797-6436",
+        adresse: "7e rue, Rouyn-Noranda, (QC), J9X 1Z9"
+    },
     meta: {
         seperator: " - "
     },
