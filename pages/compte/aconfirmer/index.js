efc8cbdc81bbd7040016daab7dbb51661b54e7f9
mamarmite <marcandre@mamarmite.com>
2024-01-19 16:31:30 -0500

    Changement de la méthode pour le resend du courriel de confirmation.

--- pages/compte/aconfirmer/index.js
+++ pages/compte/aconfirmer/index.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import { useContext } from 'react'
+import React, {useContext} from 'react'
 import AuthenticationMessage from '@/auth/components/Commons/AuthenticationMessage/AuthenticationMessage'
-import { useFormUtils } from '@/src/hooks/useFormUtils/useFormUtils'
-import { externalApiRequest } from '@/src/hooks/http-hook'
+import {useFormUtils} from '@/src/hooks/useFormUtils/useFormUtils'
+import {clientSideExternalApiRequest} from '@/src/hooks/http-hook'
 import Input from '@/src/common/FormElements/Input/Input'
 import Button from '@/src/common/FormElements/Button/Button'
-import { MessageContext } from '@/src/common/UserNotifications/Message/Context/Message-Context'
+import {MessageContext} from '@/src/common/UserNotifications/Message/Context/Message-Context'
 
 const confirmationForm = () => {
 
@@ -22,16 +21,16 @@ const confirmationForm = () => {
     //Function copy/pasted from /verifier-compte/[token].js
     //Possibilities of removing email input and ajust formState to the user that just registered if it's the case
     const resendToken = async () => {
-        const apiResponse = await externalApiRequest(
-            "/verify-account/resend",
+
+        const apiResponse = await clientSideExternalApiRequest("/verify-account/resend",
             {
                 body: JSON.stringify({data: { email: formState.inputs.email.value }}),
-            }
-        );
+            })
+
         msg.addMessage({ 
             text: "Un email de confirmation a été envoyé s'il s'agit d'un courriel associé à un compte",
             positive: true
-        })
+        });
     }
 
     return (
