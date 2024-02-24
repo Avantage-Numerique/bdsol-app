6fce90be8b5503d6ff4038d59b5abdc807d0aa15
mamarmite <marcandre@mamarmite.com>
2024-02-23 19:27:39 -0500

    (ajustement) Cookie paramètres un peu plus stylisé

--- pages/parametres/cookies.js
+++ pages/parametres/cookies.js
@@ -29,21 +29,23 @@ const CookiesParams = () => {
             <section className={`container pt-5`}>
                 <div className="row">
                     <div className={"col"}>
+                        <ul className={"list-group"}>
                         {Object.keys(auth.cookiesChoices).map((key, index) => {
                                 return (
-                                    <div key={`cookiesChoices${index}`}>
-                                        <h2>
-                                            {lang[`cookie${key.capitalize()}`]} : {auth.cookiesChoices[key] === true ? "Oui": "Non"}
-                                        </h2>
-                                        <hr />
-                                    </div>
+                                    <li className={"list-group-item d-flex justify-content-between align-items-center"} key={`cookiesChoices${index}`}>
+                                        <h4>
+                                            {lang[`cookie${key.capitalize()}`]}
+                                        </h4>
+                                        <span className={`alert alert-${auth.cookiesChoices[key] === true ? "success" : "danger"} m-0`}>{auth.cookiesChoices[key] === true ? lang.cookiePositive : lang.cookieNegative}</span>
+                                    </li>
                                 );
-                            })
+                        })
                         }
+                        </ul>
                     </div>
                 </div>
                 <div className="row">
-                    <div className={"col d-grid column"}>
+                    <div className={"col d-grid column my-5"}>
                         <Button onClick={changeChoices} color={"secondary"}>Changer mon choix</Button>
                     </div>
                 </div>
