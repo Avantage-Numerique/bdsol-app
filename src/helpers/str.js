9ae18e0b41b0aa2f1241d075ea7505df81369954
mamarmite <marcandre@mamarmite.com>
2024-05-06 16:06:05 -0400

    (ajout) : Page meta truncate maintenant les trucs à la longueur max désiré à la place d'ajouter un console log.

--- src/helpers/str.js
+++ src/helpers/str.js
@@ -4,3 +4,12 @@ export const removeHtml = (str) => {
     }
     return str;
 }
+
+// tooked from : https://stackoverflow.com/questions/1199352/smart-way-to-truncate-long-strings
+export const truncate = (str, n, useWordBoundary) => {
+    if (str.length <= n) { return str; }
+    const subString = str.slice(0, n-1); // the original check
+    return (useWordBoundary
+        ? subString.slice(0, subString.lastIndexOf(" "))
+        : subString) + "&hellip;";
+};
