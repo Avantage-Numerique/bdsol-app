53317f084292a166b5cef6d0c0f8c8770f71779f
mamarmite <marcandre@mamarmite.com>
2024-05-17 13:46:41 -0400

    (ajout) : Ajout des statistique dans les metas de taxonomies au front et au backend.

--- src/DataTypes/Taxonomy/components/layout/simple/TaxonomySimple.js
+++ src/DataTypes/Taxonomy/components/layout/simple/TaxonomySimple.js
@@ -1,12 +1,23 @@
+import Link from "next/link";
+import React from "react";
+import styles from './taxonomy-simple.module.scss';
 
-
-
-const TaxonomySimple = (props) => {
+const TaxonomySimple = ({taxonomy}) => {
 
     return (
-        <div>
-
-        </div>
+        <Link
+            className={`border d-flex justify-content-between align-items-center w-100 p-1 p-md-2 rounded ${styles["list-tag"]}`}
+            href={`/categories/${taxonomy.category}/${taxonomy.slug}`}
+        >
+            <span>{taxonomy.name}</span>
+            {
+                taxonomy.meta?.statistics?.count > 0 ?
+                    <span className={"badge bg-secondary"}>{taxonomy.meta?.statistics?.count}</span>
+                    :
+                    ""
+            }
+        </Link>
     )
-    
 }
+
+export default TaxonomySimple;
