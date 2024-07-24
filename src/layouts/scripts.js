5bfe7b1f36591fad6d2e8df9795a2b1f1591f530
mamarmite <marcandre@mamarmite.com>
2024-07-24 17:36:48 -0400

    (ajout) : variable d'environnement dans l'exemple. Ajustement du package.json et lock

--- src/layouts/scripts.js
+++ src/layouts/scripts.js
@@ -8,3 +8,16 @@ const footerStaticScripts = () => {
         </script>
     )
 }
+
+/*
+<!-- Matomo Tag Manager -->
+<script>
+  var _mtm = window._mtm = window._mtm || [];
+  _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+  (function() {
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://stats.avnu.ca/js/container_mde7KP9C_staging_b635b357447c6d9a11e3e6c0.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Tag Manager -->
+ */
