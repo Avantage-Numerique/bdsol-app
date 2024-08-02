c5adcb71ab596ac55956c8a80511e8f00ece1768
mamarmite <marcandre@mamarmite.com>
2024-08-02 18:45:06 -0400

    Suppresion de note non nécessaire maintenant!

--- src/layouts/scripts.js
+++ src/layouts/scripts.js
@@ -3,21 +3,6 @@
 const footerStaticScripts = () => {
 
     return (
-        <script>
-            {/*//https://matomo.org/faq/new-to-piwik/how-do-i-start-tracking-data-with-matomo-on-websites-that-use-react/*/}
-        </script>
+        <script></script>
     )
 }
-
-/*
-<!-- Matomo Tag Manager -->
-<script>
-  var _mtm = window._mtm = window._mtm || [];
-  _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-  (function() {
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='https://stats.avnu.ca/js/container_mde7KP9C_staging_b635b357447c6d9a11e3e6c0.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Matomo Tag Manager -->
- */
