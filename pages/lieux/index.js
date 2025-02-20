966dc3a2cffb85e6d98f2cb8c4796498594c31f5
SonofaPancak <epaulard21@hotmail.com>
2025-02-20 14:52:05 -0500

    Ajout de la carte avec seulement leaflet javascript. Première itération

--- pages/lieux/index.js
+++ pages/lieux/index.js
@@ -23,6 +23,7 @@ import Head from "next/head";
 import {getType, TYPE_PLACE} from "@/DataTypes/Entity/Types";
 
 
+
 const PlacesPage = () => {
 
     const [ placeList, setPlaceList ] = useState([]);
@@ -70,9 +71,58 @@ const PlacesPage = () => {
         }[param];
     }, []);
 
-    return (
+    const MapComponent = () => {
+        useEffect(() => {
+            // La variable L est maintenant accessible car Leaflet est chargé par le CDN
+            const map = L.map('map').setView([48.236,-79.023], 13); // Position de la carte et zoom
+      
+            // Ajouter un fond de carte (par exemple OpenStreetMap)
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
 
+            var marker = L.marker([48.23, -79.0]).addTo(map);
+            var circle = L.circle([48.236, -79.023], {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.09,
+                radius: 5500
+            }).addTo(map);
+            var polygon = L.polygon([
+                [48.28, -79.09],
+                [48.18, -79.09],
+                [48.18, -79.02]
+            ]).addTo(map);
+            marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+            circle.bindPopup("I am a circle.");
+            polygon.bindPopup("I am a polygon.");
+            var popup = L.popup();
+            function onMapClick(e) {
+                popup
+                    .setLatLng(e.latlng)
+                    .setContent("You clicked the map at " + e.latlng.toString())
+                    .openOn(map);
+            }
+            map.on('click', onMapClick);
+        }, []);
+      
+        return (
+          <div id="map" style={{ height: '400px'}}></div> // Div pour la carte avec une hauteur définie
+        );
+    };
+
+
+    return (
         <div>
+            <Head>
+                <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+                    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+                    crossorigin=""/>
+                <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+                    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+                    crossorigin=""></script>
+            </Head>
+
             <PageMeta 
                 title={getTitle([type.labelPlural])}
                 description={lang.places__description}
@@ -85,8 +135,8 @@ const PlacesPage = () => {
             >
                 <Breadcrumbs className={"pt-2"} route={AppRoutes.places} getLabelGenerator={getLabelGenerator} />
             </PageHeader>
-
                 <div className="container">
+                <MapComponent/>
                     <div className="row my-4 py-4">
 
                         {/* Feed section */}
