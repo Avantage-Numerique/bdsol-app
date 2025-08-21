40b73132ef0c3e94fdfdcdcd4886fa0fa8f25593
SonofaPancak <epaulard21@hotmail.com>
2025-08-21 14:12:05 -0400

    Fix erreur console pour keys du radioButton, fix des borderLeft sur certains composant en mode édition single

--- src/DataTypes/Event/component/layout/single/EventSingleView.js
+++ src/DataTypes/Event/component/layout/single/EventSingleView.js
@@ -144,6 +144,7 @@ const EventSingleView = ({data}) => {
                         <SingleInfo 
                             title={lang.producer}
                             isSubtitle
+                            noCardLayout
                         >
                             {model.organizer && 
                                 <EntitiesTagGrid 
