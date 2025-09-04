1c40097a9c14008160e04e82c161ca75ece0afc5
SonofaPancak <epaulard21@hotmail.com>
2025-09-04 12:17:45 -0400

    Ajout du param redirect dans ssrCanAccess

--- src/authentification/permissions/ssrCanAccess.js
+++ src/authentification/permissions/ssrCanAccess.js
@@ -1,17 +1,27 @@
-import {defaultSessionData} from "@/auth/context/auth-context";
+import { defaultSessionData } from "@/auth/context/auth-context";
 
-export const ssrCanAccess = async ({req}) => {
+export const ssrCanAccess = async ({ req }) => {
     const user = req.session.user;
 
     // redirect if the user isn't logged in.
     if (!user || !user.isLoggedIn || !user.tokenVerified) {
+
+        //Catch where user was for redirection
+        const referer = req.headers.referer;
+        let refererPath = "/"
+        if (referer) {
+            const refererUrl = new URL(referer);
+            console.log(refererUrl)
+            refererPath = refererUrl.pathname + refererUrl.search;
+        }
+
         return {
             redirect: {
                 permanent: false,
-                destination: "/compte/connexion"
+                destination: "/compte/connexion"+`?redirect=${encodeURI(refererPath)}`
             },
             props: {
-                user: {...defaultSessionData},
+                user: { ...defaultSessionData },
             }
         };
     }
