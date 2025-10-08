15d061aa8d741adc6d537a18e5547c37d9e48aac
Marc-André Martin <marcandre@mamarmite.com>
2025-10-08 19:08:55 -0400

    Ajout d'un checkup pour le model dans le componenent Entitylink pour tester le lint et faire 1 commit 2 features.

--- src/DataTypes/Entity/layouts/EntityLink.js
+++ src/DataTypes/Entity/layouts/EntityLink.js
@@ -1,8 +1,8 @@
-import {getModelFromType, getType} from "@/DataTypes/Entity/Types";
+import { getModelFromType, getType } from "@/DataTypes/Entity/Types"
 
-import React from "react";
-import Icon from "@/common/widgets/Icon/Icon";
-import Link from "next/link";
+import React from "react"
+import Icon from "@/common/widgets/Icon/Icon"
+import Link from "next/link"
 
 /**
  *
@@ -12,28 +12,34 @@ import Link from "next/link";
  * @return {JSX.Element}
  * @constructor
  */
-const EntityLink = (props) => {
-
-    let {
-        className,
-        data
-    } = props;
+const EntityLink = props => {
+    let { className, data } = props
 
     // defaults
-    className = className ?? "";
-    const model = getModelFromType(data.type, data);
-    const type = getType(model.type);
+    className = className ?? ""
+    const model = getModelFromType(data.type, data)
 
-    //get height and width dynamicaly ?
-    return (
-        <>
-            {model &&
-                <Link href={model.singleLink} title={model.title} className={`link ${className}`}>
-                    <Icon iconName={type.icon} className="pe-1" /> {model.title}
-                </Link>
-            }
-        </>
-    )
+    if (typeof model !== "undefined") {
+        const type = getType(model.type)
+
+        //get height and width dynamicaly ?
+        return (
+            <>
+                {model && (
+                    <Link
+                        href={model.singleLink}
+                        title={model.title}
+                        className={`link ${className}`}
+                    >
+                        <Icon iconName={type.icon} className="pe-1" />{" "}
+                        {model.title}
+                    </Link>
+                )}
+            </>
+        )
+    }
+    console.error("Model is undefined for EntityLink")
+    return <span>Model is undefined</span>
 }
 
-export default EntityLink;
+export default EntityLink
