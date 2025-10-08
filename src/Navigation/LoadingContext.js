b7724780c66c509bdc0377a0b39e04adfe11c062
lolcabanon <julesses@gmail.com>
2025-10-08 17:24:11 -0400

    déplacé bouton "Toggle loading" pour le dev au milieu

--- src/Navigation/LoadingContext.js
+++ src/Navigation/LoadingContext.js
@@ -1,39 +1,39 @@
-"use client";
+"use client"
 
-import { createContext, useContext, useState } from "react";
-import { isDev } from "../helpers/configHelper";
+import { createContext, useContext, useState } from "react"
+import { isDev } from "../helpers/configHelper"
 
-const LoadingContext = createContext();
+const LoadingContext = createContext()
 
 export function LoadingProvider({ children }) {
-  const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false)
 
-  return (
-    <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
-      {isDev && (
-        <div
-          className="fixed-bottom p-3"
-          style={{
-            zIndex: "6666",
-          }}
-        >
-          <button
-            type="button"
-            className="btn btn-color-secondary"
-            onClick={() => {
-              setIsLoading(!isLoading);
-            }}
-          >
-            Toggle loading
-          </button>
-        </div>
-      )}
+    return (
+        <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
+            {isDev && (
+                <div
+                    className="position-fixed bottom-0 start-50 translate-middle p-1 opacity-75"
+                    style={{
+                        zIndex: "6666",
+                    }}
+                >
+                    <button
+                        type="button"
+                        className="btn btn-color-secondary"
+                        onClick={() => {
+                            setIsLoading(!isLoading)
+                        }}
+                    >
+                        Toggle loading
+                    </button>
+                </div>
+            )}
 
-      {children}
-    </LoadingContext.Provider>
-  );
+            {children}
+        </LoadingContext.Provider>
+    )
 }
 
 export function useLoading() {
-  return useContext(LoadingContext);
+    return useContext(LoadingContext)
 }
