575a68a545c055d91f5aa191202b4b174cc3213d
lolcabanon <julesses@gmail.com>
2025-10-22 16:43:03 -0400

    pousser le contenu dans le milieu

--- pages/imagetest/index.js
+++ pages/imagetest/index.js
@@ -3,26 +3,14 @@ import ImageCropper from '@/src/common/ImageCropper/ImageCropper';
 import MainImageDisplay from '@/src/DataTypes/common/layouts/single/defaultSections/MainImageDisplay/MainImageDisplay';
 
 const TestImage = () => {
-    return (
-        <div>
-            <div>a</div>
-            <div>a</div>
-            <div>a</div>
-            <div>a</div>
-            <div>a</div>
-            <div>a</div>
-            <div>a</div>
-            <div>a</div>
-            <div>a</div>
-            <div>a</div>
-            <div>a</div>
-            <div>a</div>
-            <div>a</div>
-            <h2>React Image Crop & Compress Demo</h2>
-            <ImageCropper />
-{/*             <MainImageDisplay buttonClasses="fs-6" mainImage={currentMainImage ?? undefined} entity={currentModel ?? undefined} setter={updateModelMainImage ?? undefined}/>
- */}        </div>
-    );
-}
+  return (
+    <div className="mt-5" style={{ height: '100vh', display: 'grid', placeContent: 'center' }}>
+      <h2>React Image Crop & Compress Demo</h2>
+      <ImageCropper />
+      {/*             <MainImageDisplay buttonClasses="fs-6" mainImage={currentMainImage ?? undefined} entity={currentModel ?? undefined} setter={updateModelMainImage ?? undefined}/>
+       */}{' '}
+    </div>
+  );
+};
 
 export default TestImage;
