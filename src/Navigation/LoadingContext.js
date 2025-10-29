e42f9c67ded31520b7b76236a98f7a6abde6d17d
lolcabanon <90099349+lolcabanon@users.noreply.github.com>
2025-10-29 14:45:48 -0400

    Enlevé le bouton toggle dans le bas

--- src/Navigation/LoadingContext.js
+++ src/Navigation/LoadingContext.js
@@ -10,25 +10,6 @@ export function LoadingProvider({ children }) {
 
     return (
         <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
-            {isDev && (
-                <div
-                    className="position-fixed bottom-0 start-50 translate-middle p-1 opacity-75"
-                    style={{
-                        zIndex: "6666",
-                    }}
-                >
-                    <button
-                        type="button"
-                        className="btn btn-color-secondary"
-                        onClick={() => {
-                            setIsLoading(!isLoading)
-                        }}
-                    >
-                        Toggle loading
-                    </button>
-                </div>
-            )}
-
             {children}
         </LoadingContext.Provider>
     )
