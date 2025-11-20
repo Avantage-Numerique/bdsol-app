7c88a9a034a0cdffa29de527c1e9e43b5a80a3d4
lolcabanon <julesses@gmail.com>
2025-11-20 15:22:32 -0500

    fix variation 3

--- src/Navigation/LinkWithLoading.js
+++ src/Navigation/LinkWithLoading.js
@@ -3,29 +3,36 @@
 import Link from "next/link";
 import { useLoading } from "./LoadingContext"; // Ton contexte custom
 
-export default function LinkWithLoading({ href, children, onClick, ...props }) {
-    const { setIsLoading } = useLoading();
+// export default function LinkWithLoading({ href, children, onClick, ...props }) {
+//     const { setIsLoading } = useLoading();
 
-    const handleClick = (e) => {
-        // Si c'est un lien externe ou un téléchargement, ne pas intercepter
-        if (e.metaKey || e.ctrlKey || e.shiftKey || (e.nativeEvent && e.nativeEvent.which === 2)) {
-            return;
-        }
+//     /**
+//      *
+//      * @param {import("react").MouseEvent} e
+//      * @returns
+//      */
+//     const handleClick = (e) => {
+//         // Si c'est un lien externe ou un téléchargement, ne pas intercepter
+//         if (e.metaKey || e.ctrlKey || e.shiftKey || (e.nativeEvent && e.nativeEvent.which === 2)) {
+//             return;
+//         }
 
-        // Démarrer le chargement avant la navigation
-        setIsLoading(true);
-    };
+//         // Démarrer le chargement avant la navigation
+//         // setIsLoading(true);
+//     };
 
-    return (
-        <Link
-            href={href}
-            onClick={(e) => {
-                handleClick(e);
-                onClick?.(); // si onclick exist
-            }}
-            {...props}
-        >
-            {children}
-        </Link>
-    );
-}
+//     return (
+//         <Link
+//             href={href}
+//             onClick={(e) => {
+//                 handleClick(e);
+//                 onClick?.(); // si onclick exist
+//             }}
+//             {...props}
+//         >
+//             {children}
+//         </Link>
+//     );
+// }
+
+export default Link;
--- src/Navigation/NavigationEvents.js
+++ src/Navigation/NavigationEvents.js
@@ -4,15 +4,50 @@ import { useEffect } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useLoading } from "./LoadingContext"; // Un contexte personnalisé pour gérer l'état de chargement
 
+import { useRouter } from "next/router";
+
 export default function NavigationEvents() {
-    const pathname = usePathname();
-    const searchParams = useSearchParams();
+    // const pathname = usePathname();
+    // const searchParams = useSearchParams();
 
     const { setIsLoading } = useLoading(); // Fonction pour mettre à jour l'état de chargement
 
-    useEffect(() => {
+    const router = useRouter();
+
+    let timer;
+
+    const beforeNavigate = () => {
+        console.log("beforeNavigate");
+
+        timer = setTimeout(() => {
+            console.log("beforeNavigate timeout");
+
+            setIsLoading(true);
+        }, 300);
+    };
+
+    const afterNavigate = () => {
+        console.log("afterNavigate");
+
+        clearTimeout(timer);
         setIsLoading(false);
-    }, [pathname, searchParams, setIsLoading]);
+    };
+
+    useEffect(() => {
+        router.events.on("routeChangeStart", beforeNavigate);
+        router.events.on("routeChangeComplete", afterNavigate);
+        router.events.on("routeChangeError", afterNavigate);
+
+        return () => {
+            router.events.off("routeChangeStart", beforeNavigate);
+            router.events.off("routeChangeComplete", afterNavigate);
+            router.events.off("routeChangeError", afterNavigate);
+        };
+    }, [router]);
+
+    // useEffect(() => {
+    //     setIsLoading(false);
+    // }, [pathname, searchParams, setIsLoading]);
 
     return null;
 }
