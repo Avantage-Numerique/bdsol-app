9160e9a014ac44987f1af1d15d5fd1580acb9b0a
lolcabanon <julesses@gmail.com>
2026-02-04 12:20:56 -0500

    rework de la page /referentiel et ajout des chaines de traductions

---
+++ src/languages/fr-ca/referentiel.js
@@ -0,0 +1,11 @@
+const referentiel = {
+    entities: "Entités",
+
+    subschemas: "Sous-schémas",
+
+    properties: "Propriétés",
+
+    relationLinks: "Liens de relations",
+};
+
+exports.referentiel = referentiel;
