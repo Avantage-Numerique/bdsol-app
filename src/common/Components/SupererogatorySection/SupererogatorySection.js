ca345ae0012882c31aa389b7004c2eb88d17b954
lolcabanon <julesses@gmail.com>
2026-04-01 11:51:37 -0400

    enlever MetaDisplay et mock jsonld

--- src/common/Components/SupererogatorySection/SupererogatorySection.js
+++ src/common/Components/SupererogatorySection/SupererogatorySection.js
@@ -33,44 +33,7 @@ export const SupererogatorySection = (props) => {
                 {model.shortDescription && <ShortDescriptionDisplay>{model.shortDescription}</ShortDescriptionDisplay>}
                 {model.keywords && <KeywordsDisplay keywords={model.keywords} />}
                 {model.meta.jsonld && <JSONLDDisplay model={model} />}
-                {model.meta && <MetaDisplay model={model} />}
             </Collapsible>
-        </>
-    );
-};
-
-/**
- * Temporary component initially used for testing the `<Collapsible/>`
- *
- * @param {SupererogatorySectionProps} props
- * @returns
- */
-const MetaDisplay = (props) => {
-    const { model } = props;
-
-    function getCircularReplacer() {
-        const ancestors = [];
-        return function (key, value) {
-            if (typeof value !== "object" || value === null) {
-                return value;
-            }
-            // `this` is the object that value is contained in,
-            // i.e., its direct parent.
-            while (ancestors.length > 0 && ancestors.at(-1) !== this) {
-                ancestors.pop();
-            }
-            if (ancestors.includes(value)) {
-                return "[Circular]";
-            }
-            ancestors.push(value);
-            return value;
-        };
-    }
-
-    return (
-        <>
-            <strong>{lang.metaContentTitle} :</strong>
-            <details>{model.meta && <pre>{JSON.stringify(model.meta, getCircularReplacer(), 2)}</pre>}</details>
         </>
     );
 };
