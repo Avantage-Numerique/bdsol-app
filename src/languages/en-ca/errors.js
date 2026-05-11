7c8c8d0fcdf4e67c182a15fbe1807ea954e33475
Marc-André Martin <marcandre@mamarmite.com>
2026-05-11 16:25:33 -0400

    Lorsqu'on load l'app, on ne fetch pas les badges si l'API est down. Ajustement du visuel de la banière avec les variables de couleurs.

--- src/languages/en-ca/errors.js
+++ src/languages/en-ca/errors.js
@@ -4,6 +4,10 @@ const errors = {
     maybeThisWouldHelp: "Peut-être qu'une de ces sections vous aidera",
 
     modelNotValid: "Model not valid",
+    apiIsUp: "EN L'api répond aux appels de l'application AVNU.",
+    apiIsUpDev: "EN Bon, l'API est up. Asteure travaille mon vaillant!",
+    apiIsDown: "EN L'api ne répond pas à nos appels. On travail sur le dossier.",
+    apiIsDownDev: "EN Anweille réveille, ton docker est pas actifs ou tu as oublié de partir l'API 8-)",
 };
 
 exports.errors = errors;
--- src/languages/fr-ca/errors.js
+++ src/languages/fr-ca/errors.js
@@ -4,4 +4,8 @@ export const errors = {
     maybeThisWouldHelp: "Vous pouvez rechercher parmi tout nos données",
 
     modelNotValid: "Les données pour ce Model n'est pas valide.",
+    apiIsUp: "L'api répond aux appels de l'application AVNU.",
+    apiIsUpDev: "Bon, l'API est up. Asteure travaille mon vaillant!",
+    apiIsDown: "L'api ne répond pas à nos appels. On travail sur le dossier.",
+    apiIsDownDev: "Anweille réveille, ton docker est pas actifs ou tu as oublié de partir l'API 8-)",
 };
