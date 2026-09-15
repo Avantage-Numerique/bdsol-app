a2471e1e74c1e499389a3e83ff97b38f5f223cd7
SonofaPancak <epaulard21@hotmail.com>
2026-09-15 14:03:59 -0400

    Modification du tableau dans /referentiel

--- pages/referentiel/index.js
+++ pages/referentiel/index.js
@@ -24,67 +24,67 @@ const ReferentialHomePage = () => {
     const baseRoute = "/ref";
     const apiUrl = process.env.API_URL;
 
-    const RefList = (ref) => {
-        if (ref === undefined) return <></>;
+    const ontologyTable = (ref) => {
+        if (ref === undefined || ref.ontologiesMetaData === undefined) return <></>;
 
         return (
             <div>
-                {Object.entries(ref).map(([sectionKey, sectionValue]) => (
-                    <div key={sectionKey}>
-                        <h3>{lang[sectionKey]}</h3>
+                <h3>Ontologies supportées</h3>
 
-                        <table className={`${styles.ref}`}>
-                            <thead>
-                                <tr>
-                                    <th>Item</th>
-                                    <th>URL</th>
-                                    <th>Description</th>
-                                </tr>
-                            </thead>
+                <table className={`${styles.ref}`}>
+                    <thead>
+                        <tr>
+                            <th>Voir la compatibilité avec AVNU</th>
+                            <th>Description</th>
+                            <th>Documentation externe</th>
+                        </tr>
+                    </thead>
 
-                            <tbody>
-                                {Object.entries(sectionValue).map(([childKey, childValue]) => {
-                                    return (
-                                        <tr key={childKey}>
-                                            <td>{childValue.label}</td>
-                                            <td>
-                                                <a href={`${apiUrl}${baseRoute}${childValue.url}`}>
-                                                    {baseRoute}
-                                                    {childValue.url}
-                                                </a>
-                                            </td>
-                                            <td>{childValue.description}</td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
-                ))}
+                    <tbody>
+                        {Object.entries(ref.ontologiesMetaData).map(([ontology, ontologyMetaData]) => (
+                            <tr key={ontology}>
+                                <td>
+                                    <a href={`${apiUrl}${baseRoute}/compatibility#${ontology}`}>
+                                        {ontologyMetaData.label}
+                                    </a>
+                                </td>
+                                <td>{ontologyMetaData.description}</td>
+                                <td>
+                                    <a href={ontologyMetaData.referentialUrl} target="_blank" rel="noopener noreferrer">
+                                        {ontologyMetaData.referentialUrl}
+                                    </a>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         );
     };
 
     return (
         <>
-            <PageMeta title={"Référentiel"} description={"Description de l'ontologie d'Avnu"} />
+            <PageMeta
+                title={"Référentiel"}
+                description={"Référentiel d'Avnu et compatibilité avec d'autres ontologies externe"}
+            />
 
             <PageHeader
                 bg={"bg-primary-lighter"}
                 textColor={"text-white"}
                 htmlTitle={"Référentiel AVNU"}
                 subTitle={
-                    "Bienvenue dans la zone de geek. Vous retrouverez des liens vers des contrées obscures de l'ontologie d'Avnu"
+                    "Bienvenue dans la zone de geek. Vous retrouverez des liens vers les contrées obscures du référentiel d'AVNU et de ses compatibilités ontologiques !"
                 }
             />
 
             <p className="py-4">
-                <a href={`${process.env.NEXT_PUBLIC_API_URL}/ref`} className="external-link fw-bold">
-                    <Icon iconName="link" className="font-bold" /> Lien vers l&apos;accueil du référentiel complet
+                <a href={`${process.env.NEXT_PUBLIC_API_URL}/ref/compatibility`} className="external-link fw-bold">
+                    <Icon iconName="link" className="font-bold" /> Lien vers l&apos;accueil du référentiel AVNU
                 </a>
             </p>
 
-            {RefList(refResponse)}
+            {ontologyTable(refResponse)}
         </>
     );
 };
